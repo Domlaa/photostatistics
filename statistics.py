0cbe3ee911b349fda2b917b9e9bde0c3ae6fe416
Domlaa <dominx@qq.com>
2025-09-03 00:10:27 +0800

    save exif info with json

--- statistics.py
+++ statistics.py
@@ -46,30 +46,34 @@ def get_raw_metadata(folder_path, exiftool_path="exiftool"):
         # print(f"cmd: {cmd}")
         result = subprocess.run(cmd, capture_output=True, text=True, encoding="utf-8")
         metadata_list = json.loads(result.stdout)
-
-        for meta in metadata_list:
-            # 按顺序拼接字段值
-            concat_str = "|".join(str(meta.get(t, "")) for t in TAGS)
-            # 生成 MD5
-            md5_value = hashlib.md5(concat_str.encode("utf-8")).hexdigest()
-
-            print(f"concat: {concat_str}, md5: {md5_value}")
-            # print(f"meta: {meta}")
-            results.append({
-                # "make": meta.get("Make"),
-                "model": meta.get("Model"),
-                "filename": Path(meta["SourceFile"]).name,
-                "datetime": meta.get("SubSecDateTimeOriginal"),
-                "aperture": meta.get("FNumber"),
-                "shutter": meta.get("ExposureTime"),
-                "iso": meta.get("ISO"),
-                "lens": meta.get("LensModel"),
-                "focal_length": parse_focal_length(meta.get("FocalLength")),
-                "md5": md5_value,
-                "filepath": meta["SourceFile"],
-            })
+        results.extend(metadata_list)
 
     return results
+
+def handle_exif_info_array(metadata_list):
+    results = []
+    for meta in metadata_list:
+        # 按顺序拼接字段值
+        concat_str = "|".join(str(meta.get(t, "")) for t in TAGS)
+        # 生成 MD5
+        md5_value = hashlib.md5(concat_str.encode("utf-8")).hexdigest()
+        print(f"concat: {concat_str}, md5: {md5_value}")
+        # print(f"meta: {meta}")
+        results.append({
+            # "make": meta.get("Make"),
+            "model": meta.get("Model"),
+            "filename": Path(meta["SourceFile"]).name,
+            "datetime": meta.get("SubSecDateTimeOriginal"),
+            "aperture": meta.get("FNumber"),
+            "shutter": meta.get("ExposureTime"),
+            "iso": meta.get("ISO"),
+            "lens": meta.get("LensModel"),
+            "focal_length": parse_focal_length(meta.get("FocalLength")),
+            "md5": md5_value,
+            "filepath": meta["SourceFile"],
+        })
+    # db_mysql.save_to_mysql(data, False)
+    # db_sqlite.save_to_sqlite(data, True)
 
 def parse_focal_length(value: str) -> float:
     if isinstance(value, str):
@@ -77,12 +81,20 @@ def parse_focal_length(value: str) -> float:
     return float(value)
 
 
+def save_json_file(json_data, filename):
+    # 保存为 JSON 文件
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(json_data, f, ensure_ascii=False, indent=4)
+
+
+
 if __name__ == "__main__":
     # data = get_raw_metadata(r"E:\\Photo2024")
 
-    data = get_raw_metadata(r"/Volumes/one/canon2025/test")
-    # db_mysql.save_to_mysql(data, False)
-    # db_sqlite.save_to_sqlite(data, True)
+    data = get_raw_metadata(r"/Volumes/one/canon2025")
+    print(f"read file: {len(data)}")
+    save_json_file(data, "canon2025.json")
+
 
 
 
